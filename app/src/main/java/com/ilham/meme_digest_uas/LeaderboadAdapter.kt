1ad6cd0dec689eb2c763390fd43681a78e3771e7
Nurleila Rahmawati Sulistyo <rnurleila@gmail.com>
2023-01-10 15:12:15 +0700

    leaderboard finished rapi

--- app/src/main/java/com/ilham/meme_digest_uas/LeaderboadAdapter.kt
+++ app/src/main/java/com/ilham/meme_digest_uas/LeaderboadAdapter.kt
@@ -30,7 +30,8 @@ class LeaderboadAdapter ( val leaderboards: ArrayList<Leaderboard>): RecyclerVie
         if(leaderboards[position].privacy!=1){
         holder.v.txtNameLeaderboard.text = leaderboards[position].first_name +" "+ leaderboards[position].last_name
         }else{
-
+            //belum di substring ***
+            holder.v.txtNameLeaderboard.text = leaderboards[position].first_name +" "+ leaderboards[position].last_name
         }
         holder.v.btnLikeLeaderboard.text = leaderboards[position].total_likes.toString()
 
