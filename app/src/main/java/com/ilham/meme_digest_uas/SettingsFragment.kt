ef4d1f42783121d2d2ca716d353f73c2d3a0ec6e
Nurleila Rahmawati Sulistyo <rnurleila@gmail.com>
2023-01-10 18:05:21 +0700

    Settings (kurang edit avatar)

--- app/src/main/java/com/ilham/meme_digest_uas/SettingsFragment.kt
+++ app/src/main/java/com/ilham/meme_digest_uas/SettingsFragment.kt
@@ -1,32 +1,79 @@
 package com.ilham.meme_digest_uas
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_settings.*
+import org.json.JSONObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var firstName =""
+    private var lastName = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+        var sharedName= "com.ilham.meme_digest_uas"
+        var shared = this.requireActivity().getSharedPreferences(sharedName, Context.MODE_PRIVATE)
+        var userId = shared?.getString("ACTIVEUSERID", null)
+        Log.d("userid",userId.toString())
+
+
+//        var username =""
+//        var regist_date=""
+//        var avatar = ""
+//        var privacy = 0
+
+        val q = Volley.newRequestQueue(activity)
+        val url = "https://ubaya.fun/flutter/160719052/nmp/detail_user.php"
+        var stringRequest = object:StringRequest(
+            Request.Method.POST, url,
+            Response.Listener<String> {
+                Log.d("apiresult", it)
+                val obj = JSONObject(it)
+                if (obj.getString("result") == "success") {
+                    firstName=obj.getString("first_name")
+                    Log.d("firstname",firstName)
+                    txtFirstLastName.text =  obj.getString("first_name")+" "+ obj.getString("last_name")
+                    txtActiveMonth.text = "Active since "+obj.getString("registration_date")
+                    txtUsernameSettings.text="@"+obj.getString("username")
+                    txtFirstNameSettings.setText( obj.getString("first_name"))
+                    txtLastNameSettings.setText(obj.getString("last_name"))
+                    if(obj.getString("privacy")=="1"){
+                        checkBoxHideMyName.isChecked=true
+                    }
+
+                }else{
+                    Toast.makeText(activity, "Error", Toast.LENGTH_SHORT).show()
+                }
+            },
+            Response.ErrorListener {
+                Log.e("apiresult", it.message.toString())
+            }
+        ){
+            override fun getParams(): MutableMap<String, String> {
+                val params = HashMap<String, String>()
+                params["id"] = userId.toString()
+                return params
+            }
         }
+        q.add(stringRequest)
+//        txtActiveMonth.text="Active since "+ regist_date
+//        txtUsernameSettings.text=username
+
     }
 
     override fun onCreateView(
@@ -35,6 +82,50 @@ class SettingsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var sharedName= "com.ilham.meme_digest_uas"
+        var shared = this.requireActivity().getSharedPreferences(sharedName, Context.MODE_PRIVATE)
+        var userId = shared?.getString("ACTIVEUSERID", null)
+        var privacy=0;
+
+        btnSaveChanges.setOnClickListener {
+            firstName=txtFirstNameSettings.text.toString()
+            lastName=txtLastNameSettings.text.toString()
+            if(checkBoxHideMyName.isChecked==true){
+                privacy=1;
+            }
+            val q = Volley.newRequestQueue(activity)
+            val url = "https://ubaya.fun/flutter/160719052/nmp/updateProfile.php"
+            var stringRequest = object:StringRequest(
+                Request.Method.POST, url,
+                Response.Listener<String> {
+                    Log.d("apiresult", it)
+                    val obj = JSONObject(it)
+                    if (obj.getString("result") == "success") {
+                        Toast.makeText(activity, "Update Succes", Toast.LENGTH_SHORT).show()
+                        txtFirstLastName.setText(firstName+" "+lastName)
+                    }else{
+                        Toast.makeText(activity, "Error", Toast.LENGTH_SHORT).show()
+                    }
+                },
+                Response.ErrorListener {
+                    Log.e("apiresult", it.message.toString())
+                }
+            ){
+                override fun getParams(): MutableMap<String, String> {
+                    val params = HashMap<String, String>()
+                    params["first_name"] = firstName
+                    params["last_name"] = lastName
+                    params["privacy"] = privacy.toString()
+                    params["users_id"] = userId.toString()
+                    return params
+                }
+            }
+            q.add(stringRequest)
+        }
     }
 
     companion object {
@@ -51,8 +142,6 @@ class SettingsFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             SettingsFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
